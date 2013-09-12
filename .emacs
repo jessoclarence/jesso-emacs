6022052174773eeabc4c406a37655d6135d9ab25
Jesso Clarence <jessoclarence@gmail.com>
2013-09-12 20:28:54 +0530

    Initial commit.

---
+++ .emacs
@@ -0,0 +1,78 @@
+;;; package --- Jesso's init file
+
+;;; Commentary:
+; My experiments Sept 11, 2013
+
+;;; Code:
+
+; Show line numbers
+(global-linum-mode 1)
+
+; Remove icon-bar
+(tool-bar-mode -1)
+
+; Delete trailing whitespace on file save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+; load theme
+(load-theme 'tango-dark t)
+
+; Save buffer list on exit
+(require 'desktop)
+  (desktop-save-mode 1)
+  (defun my-desktop-save ()
+    (interactive)
+    ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+    (if (eq (desktop-owner) (emacs-pid))
+        (desktop-save desktop-dirname)))
+  (add-hook 'auto-save-hook 'my-desktop-save)
+
+; Full screen open
+(defun toggle-fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+	    		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+)
+(toggle-fullscreen)
+
+; Enable IDO mode
+(require 'ido)
+(ido-mode t)
+
+; Enable jedi for python
+(add-hook 'python-mode-hook 'auto-complete-mode)
+(add-hook 'python-mode-hook 'jedi:ac-setup)
+(setq jedi:complete-on-dot t)
+
+; Enable flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+; Colored terminal in emacs "M-x term" to open
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+; el-get - package manager
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+(el-get 'sync)
+
+; Initialize autocomplete plugin
+(add-to-list 'load-path "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
+
+; Enable autopair
+(require 'autopair)
+(autopair-global-mode) ;; to enable in all buffers
+
+(provide '.emacs)
+;;; .emacs ends here
---
+++ .emacs.d/el-get/.status.el
@@ -0,0 +1,48 @@
+((auto-complete status "installed" recipe
+		(:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
+		       (popup fuzzy)))
+ (autopair status "installed" recipe
+	   (:name autopair :website "https://github.com/capitaomorte/autopair" :description "Autopair is an extension to the Emacs text editor that automatically pairs braces and quotes." :type github :pkgname "capitaomorte/autopair" :features autopair))
+ (cl-lib status "installed" recipe
+	 (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+ (color-theme status "installed" recipe
+	      (:name color-theme :description "An Emacs-Lisp package with more than 50 color themes for your use. For questions about color-theme" :website "http://www.nongnu.org/color-theme/" :type http-tar :options
+		     ("xzf")
+		     :url "http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz" :load "color-theme.el" :features "color-theme" :post-init
+		     (progn
+		       (color-theme-initialize)
+		       (setq color-theme-is-global t))))
+ (color-theme-twilight status "installed" recipe
+		       (:name color-theme-twilight :description "Twilight color theme for GNU Emacs inspired by TextMate." :type github :pkgname "crafterm/twilight-emacs" :depends color-theme :prepare
+			      (autoload 'color-theme-twilight "color-theme-twilight" "color-theme: twilight" t)))
+ (ctable status "installed" recipe
+	 (:name ctable :description "Table Component for elisp" :type github :pkgname "kiwanami/emacs-ctable"))
+ (dash status "installed" recipe
+       (:name dash :description "A modern list api for Emacs. No 'cl required." :type github :pkgname "magnars/dash.el"))
+ (deferred status "installed" recipe
+   (:name deferred :description "Simple asynchronous functions for emacs lisp" :website "https://github.com/kiwanami/emacs-deferred" :type github :pkgname "kiwanami/emacs-deferred" :features "deferred"))
+ (el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+ (epc status "installed" recipe
+      (:name epc :description "An RPC stack for Emacs Lisp" :type github :pkgname "kiwanami/emacs-epc" :depends
+	     (deferred ctable)))
+ (f status "installed" recipe
+    (:name f :website "https://github.com/rejeep/f.el" :description "Modern API for working with files and directories in Emacs" :type github :pkgname "rejeep/f.el"))
+ (flycheck status "installed" recipe
+	   (:name flycheck :type github :pkgname "lunaryorn/flycheck" :description "On-the-fly syntax checking extension" :depends
+		  (s dash cl-lib f pkg-info)))
+ (fuzzy status "installed" recipe
+	(:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
+ (jedi status "installed" recipe
+       (:name jedi :description "An awesome Python auto-completion for Emacs" :type github :pkgname "tkf/emacs-jedi" :build
+	      (("make" "requirements"))
+	      :build/berkeley-unix
+	      (("gmake" "requirements"))
+	      :submodule nil :depends
+	      (epc auto-complete)))
+ (pkg-info status "installed" recipe
+	   (:name pkg-info :description "Provide information about Emacs packages." :type github :pkgname "lunaryorn/pkg-info.el" :depends s))
+ (popup status "installed" recipe
+	(:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :pkgname "auto-complete/popup-el"))
+ (s status "installed" recipe
+    (:name s :description "The long lost Emacs string manipulation library." :type github :pkgname "magnars/s.el" :features s)))
